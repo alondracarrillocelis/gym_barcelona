3c39ae5ab22ebad7359f4c52c2cb0848f5edd656
alondracarrillocelis <alondra_3127200055@utd.edu.mx>
2025-07-04 12:48:14 -0600

    Updates

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import { Redirect } from 'expo-router';
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-
-export default function HomeScreen() {
-  useFrameworkReady();
-  
-  return <Redirect href="/(tabs)" />;
-}
